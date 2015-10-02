32d07e3125f7aee75961ecae33e9a32acd977c1e
Dravucz <adravucz@shaw.ca>
2015-10-01 20:53:30 -0600

    My attempt at trying to test a Regression model in spark
    Only problem is that I need to figure out how to install the numpy
    library properly. If I can do that, then I should be able to make some
    kind of regression algorithm at least and then go to an ARMA.

--- Backend/ARMA algorithm.py
+++ Backend/ARMA algorithm.py
@@ -1,1 +1,26 @@
 #ARMA algorithm model for project
+#taken from NWP data set 00
+#starting with a simple linear regression tutorial example from
+#http://spark.apache.org/docs/latest/mllib-linear-methods.html#regression
+
+from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD, LinearRegressionModel
+# Load and parse the data
+
+def parsePoint(line):
+    values = [float(x) for x in lin.replace(',',' ').split(' ')]
+    return LabeledPoint(values[0], values[1:])
+
+data = sc.textFile("data/mllib/ridge-data/lpsa.data")
+parsedData = data.map(parsePoint)
+
+# Build the model
+model = LinearRegressionWithSGD.train(parsedData)
+
+# Evaluate the model on training data
+valuesAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
+MSE = valuesAndPreds.map(lambda (v, p): (v - p)**2).reduce(lambda x, y: x + y) / valuesAndPreds.count()
+print("Mean Squared Error = " + str(MSE))
+
+# Save and load model
+model.save(sc, "myModelPath")
+sameModel = LinearRegressionModel.load(sc, "myModelPath")
