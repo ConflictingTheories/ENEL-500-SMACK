8f9c2c4c26eafef3d6850ed65485ffaffc6d11b6
Dravucz <adravucz@shaw.ca>
2015-11-13 12:09:00 -0700

    CSV test update
    Figured out how to put specific values from a CSV into an RDD. In this
    case I made a function called load_X_coord that puts all the x
    coordinates into an array. Using that function you can then place it
    into an RDD. This function can easily be repeated for any other value
    needed in the CSV such as Y coordinate, Date values, etc.

--- Backend (Software Abstraction)/Test scripts/csv_test.py
+++ Backend (Software Abstraction)/Test scripts/csv_test.py
@@ -10,7 +10,7 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("local").setAppName("csv_test")
 sc = SparkContext(conf = conf)
 
-def loadRecord(line):
+def loadRecord(line): #row by row 
     """parse a CSV line"""
     input = StringIO.StringIO(line)
     reader = csv.DictReader(input, fieldnames=["At Date", "At Time", "For Date", "For Time","Runtime",
@@ -19,12 +19,11 @@ def loadRecord(line):
                                                "WDIR_TGL_10", "WDIR_TGL_40", "WDIR_TGL_80", "WDIR_TGL_120",
                                                "UGRD_TGL_10","UGRD_TGL_40", "UGRD_TGL_80", "UGRD_TGL_120",
                                                "VGRD_TGL_10", "VGRD_TGL_40", "VGRD_TGL_80","VGRD_TGL_120",
-                                               "TMP_TGL_2SPFH_TGL_2", "PRES_SFC_0", "TCDC_SFC_0", "NSWRS_SFC_0"])
+                                               "TMP_TGL_2", "SPFH_TGL_2", "PRES_SFC_0", "TCDC_SFC_0", "NSWRS_SFC_0"])
     return reader.next() #next row
 
 #load the CSV file 
 input = sc.textFile("csv/00_NWP_Sample.csv").map(loadRecord)
-num_commas = input.filter(lambda x: "name" in x).count()
 
 print num_commas
 print "Is the number of commas in this csv file"
@@ -34,6 +33,58 @@ first_element = input.first()
 print first_element
 print "This is the first row of the table"
 
+csv_array_type1 = input.collect();
+
+print csv_array[1]
+print "this is the second row of the table"
+
+def loadRecords(fileNameContents): #load all records in a given file
+    """Load all the records in a given file"""
+    input = StringIO.StringIO(fileNameContents[1])
+    reader = csv.DictReader(input, fieldnames=["At Date", "At Time", "For Date", "For Time","Runtime",
+                                                "X coordinate", "Y coordinate", "Latitude", "Longitude",
+                                               "WIND_TGL_10", "WIND_TGL_40", "WIND_TGL_80", "WIND_TGL_120",
+                                               "WDIR_TGL_10", "WDIR_TGL_40", "WDIR_TGL_80", "WDIR_TGL_120",
+                                               "UGRD_TGL_10","UGRD_TGL_40", "UGRD_TGL_80", "UGRD_TGL_120",
+                                               "VGRD_TGL_10", "VGRD_TGL_40", "VGRD_TGL_80","VGRD_TGL_120",
+                                               "TMP_TGL_2", "SPFH_TGL_2", "PRES_SFC_0", "TCDC_SFC_0", "NSWRS_SFC_0"])
+    return reader
+
+fullFileData = sc.wholeTextFiles("csv/00_NWP_Sample.csv").flatMap(loadRecords)
+
+csv_array_type2 = fullFileData.collect()
+
+def load_X_coord(fileNameContents):
+    """load all x coordinates"""
+    input = StringIO.StringIO(fileNameContents[1])
+    reader = csv.DictReader(input, fieldnames=["At Date", "At Time", "For Date", "For Time","Runtime",
+                                                "X coordinate", "Y coordinate", "Latitude", "Longitude",
+                                               "WIND_TGL_10", "WIND_TGL_40", "WIND_TGL_80", "WIND_TGL_120",
+                                               "WDIR_TGL_10", "WDIR_TGL_40", "WDIR_TGL_80", "WDIR_TGL_120",
+                                               "UGRD_TGL_10","UGRD_TGL_40", "UGRD_TGL_80", "UGRD_TGL_120",
+                                               "VGRD_TGL_10", "VGRD_TGL_40", "VGRD_TGL_80","VGRD_TGL_120",
+                                              "TMP_TGL_2", "SPFH_TGL_2", "PRES_SFC_0", "TCDC_SFC_0", "NSWRS_SFC_0"])
+
+    X = [0]*8384 
+    i = 0
+    for row in reader:
+        X[i] = row['X coordinate']
+        print row['X coordinate']
+        i = i + 1
+    return X
+
+X = sc.wholeTextFiles("csv/00_NWP_Sample.csv").flatMap(load_X_coord)
+
+x_coords = X.collect()
+
+#print x_coords
+#print "column of x coordinates"
+
+print x_coords[0]
+print "first x coordinate entry"
+
+print x_coords[356] #356 + 1 since the entry starts at 0
+print "357th x coordinate entry"
 
 
 
