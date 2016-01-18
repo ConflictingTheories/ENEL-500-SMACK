dde4bc76fe379f9c12f3287ee8105f7b7041c6f4
ConflictingTheories <confidential.inc@gmail.com>
2016-01-17 21:55:04 -0700

    Cron File Separation
    Separated and Stored Cron Files For easier Access

---
+++ Deployment/DEPRECATED/CRON/NWP_Data/ret_nwp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#--------------------------------------------------------
+# 				SMACK ENERGY FORECASTING 
+#--------------------------------------------------------
+#			-  Data Retrieval Script - NWP -
+#--------------------------------------------------------
+# Temporary Working Directory
+TMP_DIR=$SMACK_DIR_TMP/nwp-load
+# Check for Existence
+if ![ -e \$TMP_DIR ]; then
+	mkdir \$TMP_DIR
+fi 
+# Move into Tmp Directory
+cd \$TMP_DIR
+# Begin Downloading Files from NWP (for Recent Time)
+#
+# For all needed variables, download file for time
+#	* make list of variables
+#	* iterate through list
+#	* use current time and known time stamp
+#	* download and store into tmp directory
+#
+T = time
+echo -e "\nret_nwp.sh - run @ \$T\n" >> $CRON_PATH/log/nwp-load.log
---
+++ Deployment/DEPRECATED/CRON/NWP_Data/str_nwp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#--------------------------------------------------------
+# 				SMACK ENERGY FORECASTING 
+#--------------------------------------------------------
+#			-  Data Storage Script - NWP -
+#--------------------------------------------------------
+# Temporary Working Directory
+TMP_DIR=$SMACK_DIR_TMP/nwp-load
+# Check for Existence
+if ![ -e \$TMP_DIR ]; then
+	mkdir \$TMP_DIR
+fi 
+# Move into Tmp Directory
+cd \$TMP_DIR
+# Begin Uploading Files from to Swift Storage (for recent time)
+#
+# For each file within the directory - upload to swift
+#	* Check against files in directory
+#	* Make sure proper time
+#	* Upload to swift object storage
+#
+T = time
+echo -e "\nstr_nwp.sh - run @ \$T\n" >> $CRON_PATH/log/nwp-load.log
