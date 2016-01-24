6143f32173e6eb2c75e3e7985952a1a5e52e529e
ConflictingTheories <confidential.inc@gmail.com>
2016-01-24 01:47:49 -0700

    Some escape characters

--- Deployment/STABLE/INIT/deployment_scripts/nwp_deployment.sh
+++ Deployment/STABLE/INIT/deployment_scripts/nwp_deployment.sh
@@ -361,45 +361,45 @@ declare -r nwp_srv="http://dd.weather.gc.ca/model_hrdps/west/grib2"
 # Readout Times
 declare -r -a nwp_tz=( "00" "06" "12" "18" )
 # Sections
-declare -r -a nwp_sec=( "000" "001" "002" "003" "004" "005" "006" \
-						"007" "008" "009" "010" "011" "012" "013" \
-						"014" "015" "016" "017" "018" "019" "020" \
-						"021" "022" "023" "024" "025" "026" "027" \
-						"028" "029" "030" "031" "032" "033" "034" \
-						"035" "036" "037" "038" "039" "040" "041" \
+declare -r -a nwp_sec=( "000" "001" "002" "003" "004" "005" "006" \\
+						"007" "008" "009" "010" "011" "012" "013" \\
+						"014" "015" "016" "017" "018" "019" "020" \\
+						"021" "022" "023" "024" "025" "026" "027" \\
+						"028" "029" "030" "031" "032" "033" "034" \\
+						"035" "036" "037" "038" "039" "040" "041" \\
 						"042" "043" "044" "045" "046" "047" "048")
 # File Prefix
 declare -r nwp_pre="CMC_hrdps_west_"
 # File Suffix
 declare -r nwp_suf="-00.grib2"
 # Date Stamp
-declare -r nwp_ds=$(date +%Y%m%d)
+declare -r nwp_ds="\$(date +%Y%m%d)"
 # Wind Variables
-declare -r -a nwp_var=( "WIND_ISBL_0050_ps2.5km_" "WIND_ISBL_0100_ps2.5km_" \
-						"WIND_ISBL_0150_ps2.5km_" "WIND_ISBL_0175_ps2.5km_" \
-						"WIND_ISBL_0200_ps2.5km_" "WIND_ISBL_0225_ps2.5km_" \
-						"WIND_ISBL_0250_ps2.5km_" "WIND_ISBL_0275_ps2.5km_" \
-						"WIND_ISBL_0300_ps2.5km_" "WIND_ISBL_0350_ps2.5km_" \
-						"WIND_ISBL_0400_ps2.5km_" "WIND_ISBL_0450_ps2.5km_" \
-						"WIND_ISBL_0500_ps2.5km_" "WIND_ISBL_0550_ps2.5km_" \
-						"WIND_ISBL_0600_ps2.5km_" "WIND_ISBL_0650_ps2.5km_" \
-						"WIND_ISBL_0700_ps2.5km_" "WIND_ISBL_0750_ps2.5km_" \
-						"WIND_ISBL_0800_ps2.5km_" "WIND_ISBL_0850_ps2.5km_" \
-						"WIND_ISBL_0875_ps2.5km_" "WIND_ISBL_0900_ps2.5km_" \
-						"WIND_ISBL_0925_ps2.5km_" "WIND_ISBL_0950_ps2.5km_" \
-						"WIND_ISBL_0970_ps2.5km_" "WIND_ISBL_0985_ps2.5km_" \
-						"WIND_ISBL_1000_ps2.5km_" "WIND_ISBL_1015_ps2.5km_" \
-						"WIND_TGL_10_ps2.5km_" "WIND_TGL_40_ps2.5km_" \
+declare -r -a nwp_var=( "WIND_ISBL_0050_ps2.5km_" "WIND_ISBL_0100_ps2.5km_" \\
+						"WIND_ISBL_0150_ps2.5km_" "WIND_ISBL_0175_ps2.5km_" \\
+						"WIND_ISBL_0200_ps2.5km_" "WIND_ISBL_0225_ps2.5km_" \\
+						"WIND_ISBL_0250_ps2.5km_" "WIND_ISBL_0275_ps2.5km_" \\
+						"WIND_ISBL_0300_ps2.5km_" "WIND_ISBL_0350_ps2.5km_" \\
+						"WIND_ISBL_0400_ps2.5km_" "WIND_ISBL_0450_ps2.5km_" \\
+						"WIND_ISBL_0500_ps2.5km_" "WIND_ISBL_0550_ps2.5km_" \\
+						"WIND_ISBL_0600_ps2.5km_" "WIND_ISBL_0650_ps2.5km_" \\
+						"WIND_ISBL_0700_ps2.5km_" "WIND_ISBL_0750_ps2.5km_" \\
+						"WIND_ISBL_0800_ps2.5km_" "WIND_ISBL_0850_ps2.5km_" \\
+						"WIND_ISBL_0875_ps2.5km_" "WIND_ISBL_0900_ps2.5km_" \\
+						"WIND_ISBL_0925_ps2.5km_" "WIND_ISBL_0950_ps2.5km_" \\
+						"WIND_ISBL_0970_ps2.5km_" "WIND_ISBL_0985_ps2.5km_" \\
+						"WIND_ISBL_1000_ps2.5km_" "WIND_ISBL_1015_ps2.5km_" \\
+						"WIND_TGL_10_ps2.5km_" "WIND_TGL_40_ps2.5km_" \\
 						"WIND_TGL_80_ps2.5km_" "WIND_TGL_120_ps2.5km_")
 # File Counter
 declare -i fcnt=0
 # Loop through all file and Retrieve
 # Time Zones
-for a in \${nwp_tz[\@]}; do
+for a in \${nwp_tz[@]}; do
 	# sections
-	for b in \${nwp_sec[\@]}; do
+	for b in \${nwp_sec[@]}; do
 		# variables
-		for c in \${nwp_var[\@]}; do
+		for c in \${nwp_var[@]}; do
 			# Generate Proper File Name
 			declare filename="\${nwp_pre}\${c}\${nwp_ds}\${a}_P\${b}\${nwp_suf}"
 			# Generate Directory
@@ -410,12 +410,12 @@ for a in \${nwp_tz[\@]}; do
 			#echo -e "Downloading: ${http_path}\n"
 			curl -s -O "\${http_path}" > /dev/null
 			# Count # of Uploads
-			\(\(fcnt=\${fcnt}+1\)\)
+			((fcnt=\${fcnt}+1))
 		done
 	done
 done
 # Log Run into History
-T=`date`
+T="\$(date)"
 touch "\${CRON_PATH}/log/nwp-load.log"
 echo -e "\nret_nwp.sh - run @ \${T}\n\tRetreived: \${fcnt} Files" >> "\$CRON_PATH/log/nwp-load.log"
 EOF
@@ -445,7 +445,7 @@ cd "\${TMP_DIR}"
 #	* Check off any missing
 #	* Download missing variables
 #
-declare T=`date`
+T="\$(date)"
 echo -e "\chk_nwp.sh - run @ \${T}\n" >> "\${CRON_PATH}"/log/nwp-load.log
 EOF
 
@@ -475,7 +475,7 @@ declare -r nwp_con="nwp"
 # Pseudo-container
 declare -r nwp_pse="grib2"
 # Create Container if Non-existent
-if [ -z "$(smack-lsdb | grep \${nwp_con})" ]; then
+if [ -z "\$(smack-lsdb | grep \${nwp_con})" ]; then
 	smack-mkdb -c "\${nwp_con}" > /dev/null
 fi
 declare -i fcnt=0
@@ -483,10 +483,10 @@ declare -i fcnt=0
 for filename in *\${nwp_ds}*.grib2; do
 	smack-put "\${STORAGE_URL}/\${nwp_con}/\${nwp_pse}/\${filename}"
 	# Count # of Uploads
-	\(\(fcnt=\${fcnt}+1\)\)
+	((fcnt=\${fcnt}+1))
 done
 # Log Recording
-T=`date`
+T="\$(date)"
 echo -e "\nstr_nwp.sh - run @ \${T}\n\tStored: \${fcnt} Files\n" >> "\${CRON_PATH}/log/nwp-load.log"
 EOF
 
@@ -512,10 +512,10 @@ cd "\${TMP_DIR}"
 declare -r nwp_ds="\$(date +%Y%m%d)"
 # Remove all Today's Files
 declare -a files="\$(ls *\${nwp_ds}*.grib2 2> /dev/null)"
-declare -i fcnt="\${\#files[\@]}";\(\(fcnt=\${fcnt}-1\)\)
+declare -i fcnt="\${\#files[\@]}";((fcnt=\${fcnt}-1))
 rm -f "*\${nwp_ds}*.grib2"
 # Logging
-T=`date`
+T="\$(date)"
 echo -e "\nclr_nwp.sh - run @ \${T}\n\tRemoved: \${fcnt} Files\n" >> $CRON_PATH/log/nwp-load.log
 EOF
 
