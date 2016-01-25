4fb0e1985b9c8a3ead1c08912424978dc2a8c0df
ConflictingTheories <confidential.inc@gmail.com>
2016-01-24 21:04:33 -0700

    fixes

--- Deployment/STABLE/INIT/deployment_scripts/nwp_deployment_min.sh
+++ Deployment/STABLE/INIT/deployment_scripts/nwp_deployment_min.sh
@@ -248,24 +248,54 @@ smack-logout > /dev/null
 EOF
 cat << EOF > $CRON_PATH/bin/str_nwp.sh
 #!/bin/bash
+#--------------------------------------------------------
+# 				SMACK ENERGY FORECASTING 
+#--------------------------------------------------------
+#			-  Data Storage Script - NWP -
+#--------------------------------------------------------
+# Declare Environment Definitions
 shopt -s expand_aliases
 source /usr/local/smack/smack-env.sh
+# Temporary Working Directory
 TMP_DIR="\${SMACK_DIR_TMP}/nwp-load"
+# Check for Existence
 if ! [ -e "\${TMP_DIR}" ]; then
 	mkdir "\${TMP_DIR}"
 fi 
+# Move into Tmp Directory
 cd "\${TMP_DIR}"
+# VARIABLE DECLARATIONS
+# Date Stamp
 declare nwp_ds="\$(date +%Y%m%d)"
+# Container
 declare nwp_con="nwp"
+# Pseudo-container
 declare nwp_pse="grib2"
-if [ "\$(smack-lsdb -l 2> /dev/null | grep \${nwp_con})" != "\${nwp_con}" ]; then
+# Create Container if Non-existent
+if ! [ "\$(smack-lsdb -l 2> /dev/null | grep \${nwp_con})" == "\${nwp_con}" ]; then
 	smack-mkdb -c "\${nwp_con}" > /dev/null
 fi
+# Gather Current List of Objects
+declare -a list=$(smack-lsdb -c \${nwp_con} 2> /dev/null | grep \${nwp_pse} | grep \${nwp_ds})
 declare -i fcnt=0
+# Loop through each file and Upload:
 for filename in *\${nwp_ds}*.grib2; do
-	smack-upload -c "\${nwp_con}" -o "\${nwp_pse}/\${filename}" -f "\${filename}"
-	((fcnt=\${fcnt}+1))
+	for n in \$list
+	do
+		if [ \$n == \$filename ]; then
+			declare -x skip="TRUE"
+		fi
+	done
+	if [ $skip == "TRUE" ]; then
+		declare -x skip=""
+		continue;
+	else
+		smack-upload -c "\${nwp_con}" -o "\${nwp_pse}/\${filename}" -f "\${filename}"
+		# Count # of Uploads
+		((fcnt=\${fcnt}+1))
+	fi
 done
+# Log Recording
 T="\$(date)"
 echo -e "\nstr_nwp.sh - run @ \${T}\n\tStored: \${fcnt} Files\n" >> "\${CRON_PATH}/log/nwp-load.log"
 smack-logout > /dev/null
